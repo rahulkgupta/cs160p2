f2b5e8d5bc7c6d7f377c8fe19af8d30be2c11363
Rahul Gupta <rahulkartgupta@gmail.com>
2012-10-06 12:08:58 -0700

    Trying to see if I can pull info from the db

--- docs/views.py
+++ docs/views.py
@@ -2,6 +2,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from docs.models import Doc
 
 def home(request):
-    return render_to_response('index.html', {},context_instance=RequestContext(request))
+    docs = Doc.objects.all()
+    return render_to_response('index.html', {'docs':docs},context_instance=RequestContext(request))
