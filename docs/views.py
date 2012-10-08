4eb8318b47a96cb4fb1f1fc80dff377c704635af
Rahul Gupta <rahulkartgupta@gmail.com>
2012-10-07 19:59:33 -0700

    Visualizing the docs

--- docs/views.py
+++ docs/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from docs.models import Doc
 
+
 def home(request):
     docs = Doc.objects.all()
     return render_to_response('index.html', {'docs':docs},context_instance=RequestContext(request))
