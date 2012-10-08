8e9497837fd6b737f1fdfd0e030491e839acc9e7
Rahul Gupta <rahulkartgupta@gmail.com>
2012-10-07 21:07:35 -0700

    Visualizing the docs

--- docs/views.py
+++ docs/views.py
@@ -3,12 +3,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from docs.models import Doc
-
-from django.utils import simplejson
+from django.core import serializers
 
 
 def home(request):
     docs = Doc.objects.all()
-    doclist = list(docs)
-    js_data = simplejson.dumps(doclist)
+    js_data = serializers.serialize("json", docs)
     return render_to_response('index.html', {'docs':docs, 'jsdocs': js_data},context_instance=RequestContext(request))
