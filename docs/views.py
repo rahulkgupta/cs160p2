ea1477db9a4313971954b2fc5d4efe6cefc0d3b8
Rahul Gupta <rahulkartgupta@gmail.com>
2012-10-09 22:23:29 -0700

    Visualizing the docs

--- docs/views.py
+++ docs/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from docs.models import Doc
 from datetime import datetime
+from django.core import serializers
 import json
 from django.core.serializers.json import DjangoJSONEncoder 
 
