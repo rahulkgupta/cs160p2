27477bf3e0e6847c55043813ab7e61c48a32913a
Rahul Gupta <rahulkartgupta@gmail.com>
2012-10-09 21:53:31 -0700

    Visualizing the docs

--- docs/views.py
+++ docs/views.py
@@ -3,20 +3,19 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from docs.models import Doc
-from django.utils import simplejson
 from datetime import datetime
-from django.core.serializers.json import DjangoJSONEncoder
- 
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder 
 
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 def home(request):
     docs = Doc.objects.all().values()
+    temp_output = serializers.serialize('python', docs)
+    output = json.dumps(temp_output, cls=DjangoJSONEncoder)
 
     # for doc in docs:
     #     doc.date = datetime.strftime(docs['date'], DATETIME_FORMAT)
-    data =  json.dumps(data, cls=DjangoJSONEncoder)
 
-    js_data = simplejson.dumps(docs)
     return render_to_response('index.html', {'docs':docs, 'jsdocs': data},context_instance=RequestContext(request))
