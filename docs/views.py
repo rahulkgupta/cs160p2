6a2587d3469fda8b18c6860ab08e2a649c7d1fdd
Rahul Gupta <rahulkartgupta@gmail.com>
2012-10-09 21:44:51 -0700

    Visualizing the docs

--- docs/views.py
+++ docs/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from docs.models import Doc
 from django.utils import simplejson
+from datetime import datetime
 
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 def home(request):
     docs = Doc.objects.all().values()
 
     for doc in docs:
-        doc.date = unicode(docs['date'])
+        doc.date = datetime.strftime(docs['date'], DATETIME_FORMAT)
 
     js_data = simplejson.dumps(docs)
     return render_to_response('index.html', {'docs':docs, 'jsdocs': js_data},context_instance=RequestContext(request))
